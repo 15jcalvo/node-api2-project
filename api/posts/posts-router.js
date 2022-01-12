7b31419cb027454b78583bba7d8837925521edbc
James_Calvo <15jcalvo@gmail.com>
2022-01-12 15:53:10 -0500

    GET endpoint added

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,17 @@ const express = require('express')
 const Post = require('./posts-model')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Post.find(req.query)
+      .then(posts => {
+        res.status(200).json(posts)
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({
+          message: 'Error retrieving the posts',
+        })
+      })
+  })
+
 module.exports = router
