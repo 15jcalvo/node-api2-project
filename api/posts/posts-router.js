1b36dcb946f5d91fb01544b6315d176f8b27144f
James_Calvo <15jcalvo@gmail.com>
2022-01-12 16:03:46 -0500

    added GET by id messages

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -22,13 +22,13 @@ router.get('/:id', (req, res) => {
         if (post) {
           res.status(200).json(post)
         } else {
-          res.status(404).json({ message: 'Post not found' })
+          res.status(404).json({ message: "The post with the specified ID does not exist" })
         }
       })
       .catch(error => {
         console.log(error)
         res.status(500).json({
-          message: 'Error retrieving the post',
+            message: "The post information could not be retrieved",
         })
       })
   })
